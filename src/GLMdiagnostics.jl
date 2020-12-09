1d2c49a85d92179551ae0d786ddcbca315838f6b
Guy Sutton <g.sutton@ru.ac.za>
2020-12-09 19:36:07 +0200

    Add plot_qq unit-tests

--- src/GLMdiagnostics.jl
+++ src/GLMdiagnostics.jl
@@ -1,5 +1,9 @@
 module GLMdiagnostics
 
-# Write your package code here.
+# Separate .jl files for each function (have to manually add file, otherwise error)
+
+# Function #1: Plot quantile-quantile plot (qqplot)
+include("plot_qq.jl")
+export plot_qq
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,60 @@
 using GLMdiagnostics
 using Test
+using Distributions
+using Random
+using DataFrames
+using StatsBase
+using GLM
+using Gadfly
 
 @testset "GLMdiagnostics.jl" begin
-    # Write your tests here.
+
+
+    ##############################################
+    # - Write tests for plot_qq
+    ##############################################
+
+    @testset "plot_qq" begin
+
+        ##########################################
+        # - Create simulated data to test function
+        ##########################################
+
+        # Set reproducible seed
+        Random.seed!(123)
+
+        # Simulate data from a Normal distribution
+        d = Normal(1, 2)
+        y = rand(d, 100)
+
+        # Assign grouping variables (5 groups of 20)
+        x = repeat([1, 2, 3, 4, 5],
+                    inner = 20,
+                    outer = 1)
+
+        df = hcat(y, x)
+        df = convert(DataFrame, df)
+        colnames = ["y","x"]
+        rename!(df, Symbol.(colnames))
+
+        ##########################################
+        # - Run linear model (one-way ANOVA)
+        ##########################################
+
+        # Run model
+        modAov = lm(@formula(y ~ 1 + x), df)
+
+        # Make qqplot for ANOVA model
+        anova_qqplot = plot_qq(modAov)
+
+        ##########################################
+        # - Test function
+        ##########################################
+
+        # Test #1
+        # - Test that object created by function is of type 'Plot'
+        @test typeof(anova_qqplot) == Plot
+
+    end # End of testing for plot_qq function
+
 end
