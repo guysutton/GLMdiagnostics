3b74711bee831bba90dbf8e7a7ce727e0fc22d09
Guy Sutton <g.sutton@ru.ac.za>
2020-12-15 15:12:38 +0200

    Replace source with package functions in plot_diagnostics

--- src/plot_diagnostics.jl
+++ src/plot_diagnostics.jl
@@ -6,17 +6,12 @@ function plot_diagnostics(model)
     # Set default plot size
     Gadfly.set_default_plot_size(21Gadfly.cm, 8Gadfly.cm)
 
-    # Source functions to plot inidividual diagnostics plots
-    include("plot_qq.jl")
-    include("plot_fit_res.jl")
-    include("plot_scale_loc.jl")
-
     # Make individual diagnostics plots
     # - These plots are the equivalent of calling plot(...) on a
     #   linear model (e.g. lm, ANOVA) in R.
-    p1 = plot_qq(model)
-    p2 = plot_fit_res(model)
-    p3 = plot_scale_loc(model)
+    p1 = GLM.diagnostics.plot_qq(model)
+    p2 = GLM.diagnostics.plot_fit_res(model)
+    p3 = GLM.diagnostics.plot_scale_loc(model)
 
     # Plot all three plots in a single graph
     all_plots = Compose.hstack(p1, p2, p3)
